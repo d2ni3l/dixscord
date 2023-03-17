a3c435fc6c6b0f22ddbed526be68af5966f1a14f
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2023-03-17 13:31:01 +0100

    added mobile toggle functionality

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const mobileToggle = document.getElementById("mobile-toggle");
 const mobileNav = document.getElementById("mobile-nav");
+const body = document.querySelector('body');
+
+
+
+document.getElementById('close-mobile-toggle').addEventListener('click', () => {
+  mobileNav.style.visibility = 'hidden';
+  body.style.overflow = 'scroll'
+})
 
 
 
 mobileToggle.addEventListener('click', () => {
-    console.log('hello')
+   mobileNav.style.visibility = 'visible';
+    body.style.overflow = 'hidden';
 })
+
+
 
