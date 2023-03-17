074050b8aea7c1266403eebae995897d6239dd90
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2023-03-17 12:09:44 +0100

    added Favico.svg

---
+++ main.js
@@ -0,0 +1,9 @@
+const mobileToggle = document.getElementById("mobile-toggle");
+const mobileNav = document.getElementById("mobile-nav");
+
+
+
+mobileToggle.addEventListener('click', () => {
+    console.log('hello')
+})
+
